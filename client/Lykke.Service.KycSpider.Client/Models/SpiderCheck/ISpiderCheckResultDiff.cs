a1e19dcf2a799b867bfdb4ee8dec3e3904df5f6e
Pavel Zhdanovich <swatoplus@gmail.com>
2018-09-04 20:28:37 +0300

    Fixed client, copied models, removed reference from code

---
+++ client/Lykke.Service.KycSpider.Client/Models/SpiderCheck/ISpiderCheckResultDiff.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Lykke.Service.KycSpider.Core.Domain.SpiderCheck
+{
+    public interface ISpiderCheckResultDiff
+    {
+        IReadOnlyCollection<ISpiderProfile> RemovedProfiles { get; }
+        IReadOnlyCollection<ISpiderProfile> AddedProfiles { get; }
+        IReadOnlyCollection<ISpiderProfilePair> ChangedProfiles { get; }
+    }
+}
